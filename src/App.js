b2599d3f8b16294a288a4bd35cff937b13000d3b
ale-aleksandra <aleksandra99j@gmail.com>
2024-02-05 13:50:16 +0000

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Aleksandra Jablonska and is open-sourced on{" "}
-        <a
-          href="https://github.com/ale-aleksandra/react-weather"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Aleksandra Jablonska and is open-sourced on{" "}
+          <a
+            href="https://github.com/ale-aleksandra/react-weather"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
